6f0937c7747a08528b355f1da31e4bf81650b68e
Zoltán Makai <makaizoltan1@gmail.com>
2024-06-14 20:54:16 +0200

    Search bar added

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
+    <div :class="['flex justify-center p-5', drawerOpen ? 'ml-48' : '']">
+      <input
+        type="text"
+        v-model="searchQuery"
+        placeholder="Search based on name..."
+        class="p-2 border rounded w-1/4 transition-margin-left duration-300 ease"
+      />
+    </div>
+
     <div
+      v-if="filteredPokemons.length > 0"
       :class="[
         drawerOpen ? 'ml-48' : '',
         'flex flex-wrap gap-5 justify-start pl-5 pt-5',
       ]"
     >
       <PokemonCard
-        v-for="pokemon in pokemons"
+        v-for="pokemon in filteredPokemons"
         :key="pokemon.id"
         :pokemon="pokemon"
       />
+    </div>
+
+    <div v-else class="text-gray-500 text-xl text-center w-full pt-5">
+      No match
     </div>
   </div>
 </template>
@@ -20,6 +34,7 @@ import API from "../src/api";
 const { drawerOpen } = inject("drawer");
 
 const pokemons = ref([]);
+const searchQuery = ref("");
 
 onMounted(async () => {
   try {
@@ -27,5 +42,10 @@ onMounted(async () => {
   } catch (error) {
     console.error("Error fetching pokemons:", error);
   }
+});
+const filteredPokemons = computed(() => {
+  return pokemons.value.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+  );
 });
 </script>
